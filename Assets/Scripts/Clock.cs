2a3b1e689dd06c0b9d51ac4a776571a05230b432
WilliamKSilva <wilz.kelvin12@gmail.com>
2024-07-27 11:13:03 -0300

    feat: add clock update starting from the current system datetime

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -1,31 +1,65 @@
+using System;
 using UnityEngine;
 
-public class Clock : MonoBehaviour {
+public class Clock : MonoBehaviour
+{
     float speed = 1.0f;
-    float angle = 6.0f;
     float minutesTimer = 0.0f;
 
     int minutesToUpdateHours = 0;
 
-    [SerializeField]
-    Transform hoursPivot,  minutesPivot, secondsPivot;
+    float hoursToDegrees = -30f;
+    float minutesToDegrees = -6f;
+    float secondsToDegrees = -6f;
 
-    void Update() {
+    [SerializeField]
+    Transform hoursPivot, minutesPivot, secondsPivot;
+
+    [SerializeField]
+    bool startFromCurrentTime;
+
+    void updateFromZero()
+    {
         // Update seconds
-        secondsPivot.rotation *= Quaternion.AngleAxis(angle * Time.deltaTime * speed, -Vector3.forward);
+        // We also could use Quaternion.Euler(0, 0, -30) to deal with the rotation
+        secondsPivot.rotation *= Quaternion.AngleAxis(secondsToDegrees * Time.deltaTime * speed, Vector3.forward);
         minutesTimer += Time.deltaTime * speed;
 
         // Update minutes
-        if (minutesTimer >= 60.0f) {
+        if (minutesTimer >= 60.0f)
+        {
             minutesTimer = 0.0f;
             minutesToUpdateHours += 1;
-            minutesPivot.rotation *= Quaternion.AngleAxis(angle, -Vector3.forward);
+            minutesPivot.rotation *= Quaternion.AngleAxis(minutesToDegrees, Vector3.forward);
         }
 
         // Update hours
-        if (minutesToUpdateHours >= 12) {
+        if (minutesToUpdateHours >= 12)
+        {
             minutesToUpdateHours = 0;
-            hoursPivot.rotation *= Quaternion.AngleAxis(angle, -Vector3.forward);
+            hoursPivot.rotation *= Quaternion.AngleAxis(hoursToDegrees, Vector3.forward);
+        }
+    }
+
+    void updateFromCurrentTime() {
+        // Local rotation is relative to the parent object
+        DateTime time = DateTime.Now;
+        int hour = time.Hour;
+        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hour * hoursToDegrees);
+
+        int minute = time.Minute;
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minute * minutesToDegrees);
+
+        int seconds = time.Second;
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, seconds * secondsToDegrees);
+    }
+
+    void Update()
+    {
+        if (startFromCurrentTime) {
+            updateFromCurrentTime();
+        } else {
+            updateFromZero();
         }
     }
 }
